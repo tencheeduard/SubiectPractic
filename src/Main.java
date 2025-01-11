91242c74e7bb6d944b2671c48b77fffb306ee3e9
Tenche Eduard <tencheeduard@gmail.com>
2025-01-11 23:43:44 +0200

    punct d) rezolvat cu streamuri

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.awt.image.ImageProducer;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,45 +13,33 @@ public class Main {
         // bufferedReader il ia pe fileReader ca parametru la constructor
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        // folosim un map pentru ca e practic un dictionar
-        Map<String, Integer> dictionary = new HashMap<>();
+        Map<String, Integer> dictionary = bufferedReader.lines()
+                // impartim liniile
+                .map((line) -> line.split("&"))
 
-        // citim prima linie
-        String line = bufferedReader.readLine();
-        while(line != null)
-        {
-            // impartim linia la fiecare &
-            String[] entries = line.split("&");
+                // collect poate transforma un stream intr-un alt fel de colectie, de data asta in map.
+                .collect(Collectors.toMap(
+                        // intai ii spunem functia pe care o foloseste ca sa seteze cheia.
+                        (line)->line[2], // alegem doar numele casei
 
-            // daca dictionarul nu contine numele casei, il adaugam, si cu punctele ei
-            if(dictionary.containsKey(entries[2]) == false)
-                dictionary.put(entries[2], Integer.parseInt(entries[4]));
-            // daca da, atunci doar adauga punctele
-            else
-                dictionary.put(entries[2], dictionary.get(entries[2]) + Integer.parseInt(entries[4]));
+                        // apoi ii spunem functia pe care o foloseste ca sa seteze valoarea
+                        (line)->Integer.valueOf(line[4]), // alegem doar numarul de puncte
 
-            // citim urmatoarea linie
-            line = bufferedReader.readLine();
-        }
+                        // apoi ii putem spune ce sa faca in caz ca cheile sunt egale (adica daca intalneste a doua oara in fisier acelasi nume de casa)
+                        (a,b)->a+b // in cazul asta, vrem sa adauge numerele de puncte, ca sa gasim totalul.
+                ));
 
-        // facem o lista din numele caselor
-        List<String> sortedHouseList = new ArrayList<>(dictionary.keySet());
 
-        // cel mai simplu algoritm de sortare, dar deloc eficient [O(n^2)]
+        // facem si aici o lista separata care contine casele in ordine descrescatoare dupa puncte.
+        List<String> sortedHouseList = dictionary.keySet().stream()
 
-        // comparam fiecare element cu fiecare celalalt element. daca cel de pe pozitia i e mai mare decat cel de pe pozitia j, le interschimbam
-        for(int i = 0; i < sortedHouseList.size(); i++)
-        {
-            for(int j = 0; j < sortedHouseList.size(); j++)
-            {
-                if(dictionary.get(sortedHouseList.get(i)) > dictionary.get(sortedHouseList.get(j)))
-                {
-                    String aux = sortedHouseList.get(j);
-                    sortedHouseList.set(j, sortedHouseList.get(i));
-                    sortedHouseList.set(i, aux);
-                }
-            }
-        }
+                // folosim metoda de .sorted(), la care ii zicem sa compare dupa valoarea (punctele) din dictionar.
+                // pentru comparatie poti folosi .compareTo(), care exista pentru aproape orice tip de date (String, Float, Integer etc.)
+
+                // daca vrei sa inversezi ordinea (crescator, descrescator), doar schimbi din care dintre ele (house1 sau house2) apelezi metoda de .compareTo().
+                // deci daca vrei aici sa fie crescator, ai face dictionary.get(house1).compareTo(dictionary.get(house2)).
+                .sorted( (house1, house2) -> dictionary.get(house2).compareTo(dictionary.get(house1)) ).toList();
+
 
 
         // la fel ca la readere, avem un FileWriter si un BufferedWriter.
@@ -65,28 +55,6 @@ public class Main {
         // sa nu uiti de flush, e foarte probabil sa nu iti scrie in fisier altfel.
         bufferedWriter.flush();
         bufferedWriter.close();
-
-
-        //// metoda mai corecta (si mult mai scurta), dar putin mai greu de inteles mai jos \/
-
-        ////  luam toate perechiile de <String, Integer> si le punem intr-o lista
-        ////  (lista e o lista de perechi)
-
-        // List<Map.Entry<String, Integer>> sortedHouseList = new ArrayList<>(dictionary.entrySet());
-
-        ////    in functia asta lambda, compari valoarea (integer-ul) de la perechea 1, cu valoarea de la perechea 2.
-        ////    rezultatul e folosit automat pentru functia de .sort()
-        ////    daca compari cu e2 si dupa cu e1, iti da descrescator
-        ////    invers iti da crescator
-        
-        // sortedHouseList.sort( (e1, e2) -> e2.getValue().compareTo(e1.getValue()) );
-
-        ////    mai tarziu ai face asa:
-
-        // for(Map.Entry<String, Integer> entry : sortedHouseList)
-        //
-        //                    // cheia e numele casei (string), valoarea e numarul de puncte (integer)
-        //            bufferedWriter.write(entry.getKey() + "#" + entry.getValue() + "\n");
 
     }
 }

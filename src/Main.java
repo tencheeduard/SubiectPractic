88fa64f1a2460cea81063bfa6b7952b6aaaebff7
Tenche Eduard <tencheeduard@gmail.com>
2025-01-11 22:16:22 +0200

    punct c) rezolvat fara streams

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,34 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("punkte.txt");
-
-
-        // Folosim scanner pentru input de la tastatura
-        Scanner input = new Scanner(System.in);
-
-        // luam primul caracter din string-ul de input si il memoram
-        String inputChar = input.next();
 
         // Avem nevoie de un FileReader si un BufferedReader.
         FileReader fileReader = new FileReader(file);
         // bufferedReader il ia pe fileReader ca parametru la constructor
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        bufferedReader.lines()
-                // inlocuieste fiecare linie cu doar ce apare dupa prima &
-                .map( (line)->line.split("&")[1] )
-                // pastram doar cele care incep cu caracterul dat de la tastatura
-                .filter( (name) -> name.startsWith(inputChar) )
-                // printam tot ce a ramas
-                .forEach( (name) -> System.out.println(name) );
+        // folosim un set pentru ca nu poate contine dubluri si le sorteaza automat
+        SortedSet<String> names = new TreeSet<>();
+
+        // citim prima linie
+        String line = bufferedReader.readLine();
+        while(line != null)
+        {
+            // impartim linia la fiecare &
+            String[] entries = line.split("&");
+
+            // adaugam numele studentului
+            names.add(entries[1]);
+
+            // citim urmatoarea linie
+            line = bufferedReader.readLine();
+        }
+
+        // printam fiecare nume
+        for(String name : names)
+            System.out.println(name);
     }
 }
